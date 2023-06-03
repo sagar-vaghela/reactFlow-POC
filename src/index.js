0a65e4563980dede25682a71527714593e0ffb9b
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2023-06-03 13:38:02 +0530

    initial setup

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <div style={{width: '100vw', height: '100vh'}}>
     <App />
+    </div>
   </React.StrictMode>
 );
 
